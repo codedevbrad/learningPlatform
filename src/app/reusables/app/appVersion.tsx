18591425d0e648fd2cdbea1c533a8d7cbd162cdc
brad lumber <bradlumber.dev@gmail.com>
2024-09-19 11:18:27 +0100

    added version to data file

--- src/app/reusables/app/appVersion.tsx
+++ src/app/reusables/app/appVersion.tsx
@@ -1,7 +1,9 @@
+import { appVersion } from "@/data"
+
 export default function AppVersion ( { size } ) {
     return (
         <span className="text-sm py-0.5 px-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semi bold rounded"> 
-            v 0.85 
+            v { appVersion } 
         </span>
     )
 }
