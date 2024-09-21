c5709e711f78869b765eaaced01cc1d1a01fb07a
brad lumber <bradlumber.dev@gmail.com>
2024-09-21 21:14:20 +0100

    displays project version using  package.json instead

--- src/app/reusables/app/appVersion.tsx
+++ src/app/reusables/app/appVersion.tsx
@@ -1,9 +1,9 @@
-import { appVersion } from "@/data"
+import { version } from '../../../../package.json'
 
 export default function AppVersion ( { size } ) {
     return (
         <span className="text-sm py-0.5 px-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semi bold rounded"> 
-            v { appVersion } 
+            v { version }
         </span>
     )
 }
